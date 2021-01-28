c525e9c910f735bbbe95b4f06735df4a7b1e6054
lvhau <levanhau150299@gmail.com>
2021-01-28 11:39:33 +0700

    Upload search page

--- src/search_movies/pages/search.js
+++ src/search_movies/pages/search.js
@@ -1,12 +1,52 @@
-import React from "react";
-import { Input, Row, Col /*{Card}*/ } from "antd";
+import React, { useState } from "react";
+import { Input, Row, Col, Card, Pagination } from "antd";
 import LayoutPage from "../components/layout";
+import LoadingData from "../components/loading";
+import { searchMovieByKeywords } from "../services/api";
+import { Link } from "react-router-dom";
+import slugify from "react-slugify";
 
-// const { Meta } = Card;
+const { Meta } = Card;
 const { Search } = Input;
-const onSearch = (value) => console.log(value);
 
 const SearchPage = () => {
+  const [loadingSearch, setLoadingSearch] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [listMovies, setListMovies] = useState([]);
+
+  const searchMovies = async (keywords = "", currentPage = 1) => {
+    setLoadingSearch(true);
+    const data = await searchMovieByKeywords(keywords, currentPage);
+    if (data) {
+      setListMovies(data.results);
+      setTotalItems(data.totalItems);
+      setLoadingSearch(false);
+
+      //Phan trang
+      if (page < 1) {
+        setPage(1);
+      } else if (page > data.total_pages) {
+        setPage(data.total_pages);
+      }
+      setLoadingSearch(false);
+    }
+  };
+
+  const changePageInput = (event) => {
+    const keyword = event.target.value;
+    setKeyword(keyword);
+  };
+
+  if (loadingSearch && listMovies.length === 0) {
+    return (
+      <LayoutPage>
+        <LoadingData />
+      </LayoutPage>
+    );
+  }
+
   return (
     <>
       <LayoutPage>
@@ -39,13 +79,53 @@ const SearchPage = () => {
                   className="search__bar"
                   placeholder="Search for a movie..."
                   size="large"
-                  onSearch={onSearch}
+                  onSearch={(val) => searchMovies(val, page)}
                   enterButton="Search"
+                  onChange={changePageInput}
+                  value={keyword}
                 />
               </div>
             </div>
           </Col>
         </Row>
+
+        <Row style={{ marginTop: "25px" }}>
+          {listMovies.map((item, index) => (
+            <Col span={6} key={index}>
+              <Link to={`/movie/${slugify(item.title)}~${item.id}`}>
+                <Card
+                  hoverable
+                  style={{
+                    width: 250,
+                    marginBottom: "20px",
+                  }}
+                  cover={
+                    <img
+                      alt={item.title}
+                      src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+                    />
+                  }
+                >
+                  <Meta title={item.title} />
+                </Card>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+
+        {listMovies.length !== 0 ? (
+          <Row style={{ marginTop: "20px", textAlign: "center" }}>
+            <Col span={24}>
+              <Pagination
+                current={page}
+                total={totalItems}
+                pageSize={20}
+                onChange={(pages) => setListMovies(keyword, pages)}
+              />
+            </Col>
+          </Row>
+        ) : null}
+        
       </LayoutPage>
     </>
   );
--- src/search_movies/services/api.js
+++ src/search_movies/services/api.js
@@ -35,6 +35,13 @@ export const getDataMoviesUpComing = async (page = 1) => {
 export const getDataMoviesById = async (id = 0) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&language=en-US&append_to_response=videos,images&include_image_language=en-US,null`;
   const response = await axios.get(url);
-  const result = await response.status === 200 ? response.data : [];
+  const result = (await response.status) === 200 ? response.data : [];
   return result;
-}
+};
+
+export const searchMovieByKeywords = async (keywords = "", page = 1) => {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${keywords}&api_key=cfe422613b250f702980a3bbf9e90716&page=${page}`;
+  const response = await axios.get(url);
+  const result = (await response.status) === 200 ? response.data : [];
+  return result;
+};
